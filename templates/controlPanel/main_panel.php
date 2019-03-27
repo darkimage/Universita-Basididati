18d7e17382f0911f09abfc3aaeb787b9dd4f08f1
Luca Faggion <luc-af@live.it>
2019-03-27 08:48:01 +0100

    starting form for project

--- templates/controlPanel/main_panel.php
+++ templates/controlPanel/main_panel.php
@@ -3,8 +3,8 @@
 <div class="row">
 <div class="col-2">
     <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-        <t-tablink id="test" status="active"><?php echo L::controlpanel_addproject ?></t-tablink>
-        <t-tablink id="home">Home</t-tablink>
+        <t-tablink id="test"><?php echo L::controlpanel_addproject ?></t-tablink>
+        <t-tablink id="home" status="active">Home</t-tablink>
         <t-tablink id="profile">Profile</t-tablink>
         <t-tablink id="messages">Messages</t-tablink>
         <t-tablink id="settings">Settings</t-tablink>
@@ -12,8 +12,8 @@
 </div>
 <div class="col">
     <div class="tab-content" id="v-pills-tabContent">
-        <t-tabcontent id="home">
-            <t-template path="/templates/forms/project_form.php" model="@{templatemodel}"></t-template>
+        <t-tabcontent id="home" status="show active">
+            Home
         </t-tabcontent>
         <t-tabcontent id="profile">
             Profileeee
@@ -24,8 +24,8 @@
         <t-tabcontent id="settings">
             Settings
         </t-tabcontent>
-        <t-tabcontent id="test" status="show active">
-            Test
+        <t-tabcontent id="test">
+            <t-template path="/templates/forms/project_form.php" model="@{templatemodel}"></t-template>
         </t-tabcontent>
         </div>
     </div>
--- templates/forms/project_form.php
+++ templates/forms/project_form.php
@@ -1,5 +1,5 @@
 <t-form action="projectController?action=add" domain="project">
 <div class ="container-fluid">
-<input type="text" required id="name" name="name" placeholder="<?php echo L::project_formname ?>">
+<input type="text" class="form-control" required id="name" name="name" placeholder="<?php echo L::project_formname ?>">
 </div>
 </t-form>
